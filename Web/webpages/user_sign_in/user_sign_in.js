72339aa32b5859fd00197c6988ac44766c9cb195
anuragsamota <anuragsamotasamota@gmail.com>
2022-02-20 12:56:37 +0530

    project-structure-change

---
+++ Web/webpages/user_sign_in/user_sign_in.js
@@ -0,0 +1,9 @@
+const usr_name_inp = document.getElementById('user-name')
+const psw_inp = document.getElementById('password')
+const login_btn = document.getElementById("login-btn")
+
+
+login_btn.onclick = ()=>{
+    console.log('click',usr_name_inp.value,psw_inp.value)
+    localStorage.setItem('user-login',usr_name_inp.value)
+}
